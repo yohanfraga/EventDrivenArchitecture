107783315aa72eb748688ea778b4089548c6b635
Yohan Fraga <yohanfragasantos@gmail.com>
2025-02-21 20:09:55 -0300

    YFS - fix files

--- EventConsumer/obj/Debug/net9.0/EventConsumer.AssemblyInfo.cs
+++ EventConsumer/obj/Debug/net9.0/EventConsumer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventConsumer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cbb2d2fe9de9e720a52b2eb53925271059ef48e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventConsumer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventConsumer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EventProducer/obj/Debug/net9.0/EventProducer.AssemblyInfo.cs
+++ EventProducer/obj/Debug/net9.0/EventProducer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventProducer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cbb2d2fe9de9e720a52b2eb53925271059ef48e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventProducer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventProducer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
